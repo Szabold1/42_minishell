4119a7772aa082579f527663d822bcdcfddab92c
Boldizsar Szabo <szboldisz@gmail.com>
2024-03-11 10:26:39 +0100

    Add comment to ft_free_str_arr

--- minishell/libft/src/ft_free_str_arr.c
+++ minishell/libft/src/ft_free_str_arr.c
@@ -6,12 +6,13 @@
 /*   By: bszabo <bszabo@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:45:45 by bszabo            #+#    #+#             */
-/*   Updated: 2024/02/28 16:48:52 by bszabo           ###   ########.fr       */
+/*   Updated: 2024/03/11 10:08:11 by bszabo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// free all strings in the array and the array itself
 void	ft_free_str_arr(char **arr)
 {
 	int	i;
