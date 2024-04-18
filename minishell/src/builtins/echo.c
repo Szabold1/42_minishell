317dd11a3c4b6d73ea9eb993aac3689903535950
Boldizsar Szabo <szboldisz@gmail.com>
2024-04-18 09:08:02 +0000

    Fix '-n' flag mistakes in echo

--- minishell/src/builtins/echo.c
+++ minishell/src/builtins/echo.c
@@ -6,24 +6,33 @@
 /*   By: bszabo <bszabo@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 12:10:27 by bszabo            #+#    #+#             */
-/*   Updated: 2024/04/17 17:33:54 by bszabo           ###   ########.fr       */
+/*   Updated: 2024/04/18 09:01:12 by bszabo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // print the argument
-static void	print_arg(char **echo_arg, int j, bool *newline)
+static void	print_arg(char **echo_arg, int i, bool *newline, bool *check_flag)
 {
-	echo_arg[j] = remove_quotes(echo_arg[j]);
-	if(j == 1 && ft_strcmp(echo_arg[j], "-n") == 0)
-		*newline = false;
-	else
+	int	j;
+
+	j = 1;
+	echo_arg[i] = remove_quotes(echo_arg[i]);
+	if (*check_flag && echo_arg[i][0] == '-')
 	{
-		printf("%s", echo_arg[j]);
-		if (echo_arg[j + 1])
-			printf(" ");
+		while (echo_arg[i][j] == 'n')
+			j++;
+		if (echo_arg[i][j] == '\0')
+		{
+			*newline = false;
+			return ;
+		}
 	}
+	*check_flag = false;
+	printf("%s", echo_arg[i]);
+	if (echo_arg[i + 1])
+		printf(" ");
 }
 
 // print the arguments of the echo command, handle the -n flag
@@ -32,12 +41,14 @@ void	ms_echo(t_data *data, int i)
 	int		j;
 	bool	newline;
 	char	**echo_arg;
+	bool	check_flag;
 
 	j = 1;
 	newline = true;
 	echo_arg = data->cmds[i]->cmd_array;
+	check_flag = true;
 	while (echo_arg[j])
-		print_arg(echo_arg, j++, &newline);
+		print_arg(echo_arg, j++, &newline, &check_flag);
 	if (newline)
 		printf("\n");
 	data->exit_status = 0;
