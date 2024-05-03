1576f39b37e13ea5fffcdf8d26d91d4f794fcba7
Boldizsar Szabo <szboldisz@gmail.com>
2024-05-03 15:16:01 +0000

    Remove exit print for tester

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bszabo <bszabo@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 13:42:31 by bszabo            #+#    #+#             */
-/*   Updated: 2024/04/25 07:27:22 by bszabo           ###   ########.fr       */
+/*   Updated: 2024/05/03 15:14:31 by bszabo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static int	main_loop(t_data *data)
 		if (g_signal == CTRL_C && g_signal--)
 			data->exit_status = 130;
 		if (!data->line)
-			return (printf("exit\n"), close(0), close(1), ERROR);
+			return (close(0), close(1), ERROR); // printf("exit\n"), ... 
 		if (ft_strlen(data->line) > 0)
 		{
 			add_history(data->line);
