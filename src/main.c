4332cfe5ddf6508a410fc6facdda2f0291e062eb
Samuel Leander Eckhard <seckhard@c2r6p1.42vienna.com>
2024-05-09 20:13:25 +0200

    for tester

--- src/main.c
+++ src/main.c
@@ -6,41 +6,29 @@
 /*   By: seckhard <seckhard@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 13:42:31 by bszabo            #+#    #+#             */
-/*   Updated: 2024/05/06 16:52:16 by seckhard         ###   ########.fr       */
+/*   Updated: 2024/05/09 20:13:04 by seckhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// read line when stdin is not a terminal
-static char	*read_line_from_stdin(void)
-{
-	char	*line;
-	char	*trimmed_line;
-
-	line = get_next_line(0);
-	if (line)
-	{
-		trimmed_line = ft_strtrim(line, "\n");
-		free(line);
-		return (trimmed_line);
-	}
-	return (NULL);
-}
-
-// main loop for minishell
-// handle signals, read line, add it to history, parse, execute, and free line
-// return ERROR if any of the steps fail
+// For tester
 static int	main_loop(t_data *data)
 {
 	while (1)
 	{
 		clean_up_loop(data);
 		sig_cases(INTERACTIVE);
-		if (isatty(0))
-			data->line = readline(NEON "minishell$ " RESET);
+		if (isatty(fileno(stdin)))
+			data->line = readline(NEON "minishell$ " RESET));
 		else
-			data->line = read_line_from_stdin();
+		{
+			char *line;
+			line = get_next_line(fileno(stdin));
+			if (line)
+				data->line = ft_strtrim(line, "\n");
+			free(line);
+		}
 		sig_cases(NON_INTERACTIVE);
 		if (g_signal == CTRL_C && g_signal--)
 			data->exit_status = 130;
@@ -50,24 +38,71 @@ static int	main_loop(t_data *data)
 		{
 			add_history(data->line);
 			if (check_line(data) == ERROR)
-				continue ;
+				continue;
 			if (parse_execute_line(data) == ERROR)
-				continue ;
+				continue;
 		}
 	}
 	return (OK);
 }
 
+// // read line when stdin is not a terminal
+// static char *read_line_from_stdin(void)
+// {
+// 	char *line;
+// 	char *trimmed_line;
+
+// 	line = get_next_line(0);
+// 	if (line)
+// 	{
+// 		trimmed_line = ft_strtrim(line, "\n");
+// 		free(line);
+// 		return (trimmed_line);
+// 	}
+// 	return (NULL);
+// }
+
+// // main loop for minishell
+// // handle signals, read line, add it to history, parse, execute, and free line
+// // return ERROR if any of the steps fail
+// static int main_loop(t_data *data)
+// {
+// 	while (1)
+// 	{
+// 		clean_up_loop(data);
+// 		sig_cases(INTERACTIVE);
+// 		if (isatty(0))
+// 			data->line = readline(NEON "minishell$ " RESET);
+// 		else
+// 			data->line = read_line_from_stdin();
+// 		sig_cases(NON_INTERACTIVE);
+// 		if (g_signal == CTRL_C && g_signal--)
+// 			data->exit_status = 130;
+// 		if (!data->line)
+// 			return (printf("exit\n"), close(0), close(1), ERROR);
+// 		if (ft_strlen(data->line) > 0)
+// 		{
+// 			add_history(data->line);
+// 			if (check_line(data) == ERROR)
+// 				continue;
+// 			if (parse_execute_line(data) == ERROR)
+// 				continue;
+// 		}
+// 	}
+// 	return (OK);
+// }
+
 // 1. check arguments
 // 2. initialize data
 // 3. main loop
-int	main(int argc, char *argv[], char *env[])
+int main(int argc, char *argv[], char *env[])
 {
-	t_data	data;
+	t_data data;
 
 	if (argc != 1 || argv[1] || !env)
 		return (ft_printf_fd(2, RED "error:" RESET
-				" minishell cannot take any arguments\n"), 1);
+									" minishell cannot take any arguments\n"),
+				1);
 	if (init(&data, env) == ERROR)
 		return (clean_up(&data), ERROR);
 	if (main_loop(&data) == ERROR)
